ea25b07d247a7573769c62ca9a1e7960ce24bb08
Shubham Shakya <shubhamshakya369@gmail.com>
2024-04-16 17:53:47 +0545

    1920-Build Array from Permutation

---
+++ Array/1920-Build Array from Permutation.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/build-array-from-permutation/
+
+from typing import List
+
+
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        val = [0] * (len(nums))
+        for i in range(len(val)):
+            val[i] = nums[nums[i]]
+        return val
+
+    def buildArray2(self, nums: List[int]) -> List[int]:
+        # one liner
+        return [nums[i] for i in nums]
+
+
+s = Solution()
+nums = [0, 2, 1, 5, 3, 4]
+print(s.buildArray2(nums))
