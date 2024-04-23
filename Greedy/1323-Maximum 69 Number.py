3dbdbbaadf3aa985914134f54cbd61ef7861d6a6
Shubham Shakya <shubhamshakya369@gmail.com>
2024-04-23 23:27:33 +0545

    1323-Maximum 69 Number

---
+++ Greedy/1323-Maximum 69 Number.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/maximum-69-number/
+
+class Solution:
+    def maximum69Number(self, num: int) -> int:
+        numm = list(str(num))
+        print(numm)
+        for index, n in enumerate(numm):
+            if n == "6":
+                numm[index] = "9"
+                break
+        return int("".join(numm))
+
+
+num = 9669
+s = Solution()
+print(s.maximum69Number(num))
